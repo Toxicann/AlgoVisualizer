21514f1632f1bbd4b786fe861b75b86051c97095
Toxicann <anushkarki007@gmail.com>
2022-01-21 17:09:18 +0545

    UI WIP

--- PathfinderVis.py
+++ PathfinderVis.py
@@ -1,40 +1,58 @@
-from os import curdir
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-from queue import PriorityQueue
-from collections import deque
 import time
-import threading
 
 from PathfinderLogic import *
 
+
 def init_pathfinder():
-    WIDTH = 500
+    WIDTH = 600
 
     root = Tk()
     root.title("Path Finding Visualizer")
-    root.maxsize(900, 900)
+    root.maxsize(1200, 613)
     root.config(bg="#19232d")
-
-    # Global Variables
-    selected_alg = StringVar()
 
     # Creating empty Canvas window (on left side for pathfinding window)
     canvas = Canvas(root, width=WIDTH, height=WIDTH, bg="gray4")
     canvas.pack(fill=Y, side=LEFT, padx=10, pady=5)
 
     # Creating Empty Frame for UI later on -  on right Side
-    UI_frame = Frame(root, width=600, height=200, bg="#19232d")
+    UI_frame = Frame(root, width=600, height=1200, bg="#19232d")
     UI_frame.pack(side=RIGHT, padx=10, pady=5)
 
-    myGui.getVal(canvas, root, UI_frame) #sending vars to Pathfinder Logic
+    myGui.getVal(canvas, root, UI_frame)  # sending vars to Pathfinder Logic
     init_grid()
-    
-    Button(UI_frame, text = "start", command=StartAlgorithm).grid(row = 1, column = 1)
+
+    # GUI WINDOW BUTTONS/ RADIOBUTTONS and other things below here:
+
+    Label(UI_frame, text="PathFinding Visualizer", bg="#19232d", fg="white", font=(
+        "Arial", 32), justify=CENTER).grid(row=0, column=0, padx=10, pady=5)
+    Label(UI_frame, text="Generate Path From Your Desired Algorithm", bg="#19232d",
+          fg="white", font=("Arial", 16), justify=CENTER).grid(row=1, column=0, padx=10, pady=5)
+
+    rbutton_frame = Frame(UI_frame, bg="#19232d", bd=1, relief=SUNKEN)
+    rbutton_frame.grid(row=2, column=0, padx=10, pady=10)
+
+    rbval = Radiobutton(rbutton_frame, text="Breadth First Algorithm", variable = 1, value= breadth_first , justify="left", selectcolor="#19232d", activebackground="#19232d", activeforeground="white",
+                bg="#19232d", fg="white", font=("Arial", 15)).grid(row=0, column=0, padx=5, pady=5, sticky="W")
+    rbval = Radiobutton(rbutton_frame, text="Depth First Algorithm", variable = 1, value= depth_first, justify="left", selectcolor="#19232d", activebackground="#19232d", activeforeground="white",
+                bg="#19232d", fg="white", font=("Arial", 15)).grid(row=1, column=0, padx=5, pady=5, sticky="W")
+    rbval = Radiobutton(rbutton_frame, text="Djkstra Algortihm", variable = 1, value= djkstra, justify="left", selectcolor="#19232d", activebackground="#19232d", activeforeground="white",
+                bg="#19232d", fg="white", font=("Arial", 15)).grid(row=0, column=1, padx=5, pady=5, sticky="W")
+    rbval = Radiobutton(rbutton_frame, text="A Star Algorithm", variable = 1, value= a_star, justify="left", selectcolor="#19232d", activebackground="#19232d", activeforeground="white",
+                bg="#19232d", fg="white", font=("Arial", 15), state="active").grid(row=1, column=1, padx=5, pady=5, sticky="W")
+
+    button_frame = Frame(UI_frame, bg="#19232d")
+    button_frame.grid(row=3, column=0, padx=10, pady=5)
+    Button(button_frame, text="GeneratePath", bg="#32414b", fg="white",
+           font=("Arial", 20), command = lambda : print(rbval)).pack(side=LEFT, padx=10, pady=5)
+    Button(button_frame, text="Reset", bg="#32414b", fg="white",
+           font=("Arial", 20), command=Reset).pack(side=RIGHT, padx=10, pady=5)
 
     root.mainloop()
 
+
 if __name__ == "__main__":
     init_pathfinder()
-
